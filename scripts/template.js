8630ee720c078a1f1b76125084ca0dbc1bcaa7b3
Ada Rose Edwards <ada@ada.is>
2016-11-22 15:57:37 +0000

    Add scripts to embed rss feeds

---
+++ scripts/template.js
@@ -0,0 +1,50 @@
+var currentScript = document.currentScript || (function () {
+	var scripts = document.getElementsByTagName( 'script' );
+	return  scripts[ scripts.length - 1 ];
+} ());
+
+var template = currentScript.getAttribute('data-template');
+var src = currentScript.getAttribute('data-src');
+var parentNode = currentScript.parentNode;
+
+function addScript(url) {
+	return new Promise(function (resolve, reject) {
+		var script = document.createElement('script');
+		script.setAttribute('src', url);
+		document.head.appendChild(script);
+		script.onload = resolve;
+		script.onerror = reject;
+	});
+}
+
+function isOk(response) {
+	if (!response.ok) {
+		return Promise.resolve(response)
+			.then(getBody)
+			.then(parse)
+			.then(function (range) {
+				var message = range.querySelector('#message');
+				throw Error(message.textContent || ('Bad response: ' + response.statusText + ' (' + response.status + ')'));
+			});
+	}
+	return response;
+}
+
+function getJSON(response) {
+	return response.json();
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	// Load the template and the contents
+	Promise.all([
+		fetch(src).then(isOk).then(getJSON),
+		!(Handlebars.templates && Handlebars.templates[template]) ? addScript('scripts/compiled-templates/' + template + '.js') : Promise.resolve()
+	])
+	.then(function (data) {
+		parentNode.innerHTML = Handlebars.templates[template]({ items: data[0] });
+	})
+	.catch(function (e) {
+		console.log(e);
+	});
+});
