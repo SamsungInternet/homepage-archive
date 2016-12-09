6ccaa9a077fb98fec4538b4722317d45a51c7187
Ada Rose Edwards <ada@ada.is>
2016-12-09 13:28:46 +0000

    Calendar template

---
+++ scripts/compiled-templates/calendar.js
@@ -0,0 +1,21 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['calendar'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"calendar-entry\">\n	<h2><a href=\""
+    + alias4(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"link","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</a></h2>\n	<p>"
+    + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "</p>\n</div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, options, buffer = "";
+
+  stack1 = ((helper = (helper = helpers.items || (depth0 != null ? depth0.items : depth0)) != null ? helper : helpers.helperMissing),(options={"name":"items","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data}),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},options) : helper));
+  if (!helpers.items) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
+  if (stack1 != null) { buffer += stack1; }
+  return buffer;
+},"useData":true});
+})();
--- scripts/template.js
+++ scripts/template.js
@@ -2,15 +2,6 @@
 /* eslint no-var:0 */
 'use strict';
 
-var currentScript = document.currentScript || (function () {
-	var scripts = document.getElementsByTagName( 'script' );
-	return scripts[ scripts.length - 1 ];
-} ());
-
-var template = currentScript.getAttribute('data-template');
-var src = currentScript.getAttribute('data-src');
-var parentNode = currentScript.parentNode;
-
 function addScript(url) {
 	return new Promise(function (resolve, reject) {
 		var script = document.createElement('script');
@@ -46,17 +37,28 @@ function parseHTML(string) {
 	return document.createRange().createContextualFragment(string);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+var currentScript = document.currentScript || (function () {
+	var scripts = document.getElementsByTagName( 'script' );
+	return scripts[ scripts.length - 1 ];
+} ());
+
+document.addEventListener('DOMContentLoaded', function () {
 
 	// Load the template and the contents
+	var self = this;
+	console.log(this.src);
 	Promise.all([
-		fetch(src).then(isOk).then(getJSON),
-		!(Handlebars.templates && Handlebars.templates[template]) ? addScript('scripts/compiled-templates/' + template + '.js') : Promise.resolve()
+		fetch(self.src).then(isOk).then(getJSON),
+		!(Handlebars.templates && Handlebars.templates[self.template]) ? addScript('scripts/compiled-templates/' + self.template + '.js') : Promise.resolve()
 	])
 	.then(function (data) {
-		parentNode.innerHTML = Handlebars.templates[template]({ items: data[0].reverse() });
+		self.parentNode.innerHTML = Handlebars.templates[self.template]({ items: data[0].reverse() });
 	})
 	.catch(function (e) {
 		console.log(e);
 	});
-});
+}.bind({
+	src: currentScript.getAttribute('data-src'),
+	parentNode: currentScript.parentNode,
+	template: currentScript.getAttribute('data-template')
+}));
